22d292e089742c07f1d966ac7004331283fd2f52
Olek <149747215+Primis1@users.noreply.github.com>
2024-08-10 20:41:38 -0400

    add proper playwright config

--- jest.config.ts
+++ jest.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
     // ...
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  modulePathIgnorePatterns: ["<rootDir>/tests/"]
+  modulePathIgnorePatterns: ["<rootDir>/e2e/"]
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 }
